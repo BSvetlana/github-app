d7635adda7aa5981097f9d37ed9f99e9aab226eb
Svetlana Bisacki <svetlanabisacki@gmail.com>
2018-05-30 16:43:19 +0200

    appSearch

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
     <v-ons-page>
       <app-toolbar/>
-
+      <div class="content">
+        <app-search :query.sync="query"></app-search>
+      </div>
     </v-ons-page>
 </template>
 
 <script>
 import AppToolbar from "./components/AppToolbar";
-
+import AppSearch from "./components/AppSearch";
 
 export default {
   components: {
     AppToolbar,
-   
+    AppSearch
   },
-
+  data() {
+    return {
+      query: ""
+    };
+  }
 };
 </script>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-ons-search-input :placeholder="placeholder" 
+                        style="width: 100%;"
+                        :value="query"
+                        @input="$emit('update:query', $event.target.value)">
+    </v-ons-search-input>
+</template>
+
+<script>
+export default {
+    props: {
+        query: String,
+        placeholder: String
+    }
+    
+}
+</script>
