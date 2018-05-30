c44e49dca17066747f72dac67aa0ab61367670c9
Svetlana Bisacki <svetlanabisacki@gmail.com>
2018-05-30 16:40:39 +0200

    appToolbar

---
+++ src/components/AppToolbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-ons-toolbar>
+        <div class="left" name="left">
+           
+        </div>
+        <div class="center">{{ title }}</div>
+
+        <div class="right" name="right">
+            
+        </div>
+    </v-ons-toolbar>
+</template>
+
+<script>
+export default {
+
+    props: {
+        title: {
+            type: String,
+          default: "GitHub App"
+        }
+}
+
+};
+</script>
