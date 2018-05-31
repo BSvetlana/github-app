5d8a7b1703e3963b8e7e81b5daed71f39429d410
Svetlana Bisacki <svetlanabisacki@gmail.com>
2018-05-31 19:45:16 +0200

    list from github

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,19 @@
         <app-search :query.sync="query">
           
         </app-search>
+
+        <v-ons-list>
+          <v-ons-list-header>Respositoris of {{ query }}</v-ons-list-header>
+            <v-ons-list-item v-for="repo in repos" :key="repo">
+            <div class="left">
+              <img class="list-item__thumbnail" src="https://avatars3.githubusercontent.com/u/21221433?v=4">
+            </div>
+            <div class="center">
+              <span class="list-item__title">{{ repo.name }}</span><span class="list-item__subtitle">{{ repo.description }}</span>
+
+            </div>
+          </v-ons-list-item>
+        </v-ons-list>
         
       </div>
     </v-ons-page>
