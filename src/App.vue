8289aa03f3ec7bb7342e797b8a8a6e35d6ceeaef
Svetlana Bisacki <svetlanabisacki@gmail.com>
2018-06-01 15:09:54 +0200

    progress-circular

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,22 @@
     <v-ons-page>
       <app-toolbar/>
       <div class="content">
-        <app-search :query.sync="query">
-          
-        </app-search>
+        <app-search :query.sync="query"></app-search>
 
-        <v-ons-list>
-          <v-ons-list-header>Respositoris of {{ query }}</v-ons-list-header>
-            <v-ons-list-item v-for="repo in repos" :key="repo">
+        <div class="container" v-if="showProgress">
+           <v-ons-progress-circular indeterminate id="progress"></v-ons-progress-circular>
+        </div>
+
+        <v-ons-list v-if="showList">
+          <v-ons-list-header>                
+            Respositoris of {{ query }}
+          </v-ons-list-header>
+            <v-ons-list-item  v-for="repo in repos" :key="repo.id">
             <div class="left">
-              <img class="list-item__thumbnail" src="https://avatars3.githubusercontent.com/u/21221433?v=4">
+              <img class="list-item__thumbnail" :src=" repo.owner.avatar_url">
             </div>
             <div class="center">
               <span class="list-item__title">{{ repo.name }}</span><span class="list-item__subtitle">{{ repo.description }}</span>
-
             </div>
           </v-ons-list-item>
         </v-ons-list>
@@ -37,19 +40,42 @@ export default {
   data() {
     return {
       query: "",
-      repos: []
-    };
+      repos: [],
+      showProgress: false
+      };
+  },
+  computed: {
+    showList() {
+      return this.repos.length > 0
+    }
   },
   watch: {
+    
     query: debounce(function() {
+      this.showProgress = true
       github.getRepos(this.query)
         .then((response) => {
           this.repos = response.data
           console.log(this.repos)
         }).catch((error) => {
           console.log(error)
-        })
+        }).finally(() => this.showProgress = false)
     }, 500)
   }
 };
 </script>
+
+<style>
+
+.container {
+  text-align: center;
+}
+
+#progress {
+  width: 100px;
+  height: 100px;
+  margin: 1.5rem
+}
+  
+</style>
+
